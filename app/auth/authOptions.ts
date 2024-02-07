27a8237ed19f5902e8dd6e9513d300d8829fe71e
Anurag Singh <anurag.singh@goniyo.com>
2024-02-07 17:40:13 +0530

    upgraded to latest version

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -9,11 +9,13 @@ const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      
     }),
+   
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: 'jwt',
+    maxAge: 1 * 24 * 60 * 60,
   },
 };
 
